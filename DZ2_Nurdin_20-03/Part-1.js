16dd01f87e5e56063edac721875de94ec1101f4f
Nurdin Djumabekov <dui.nurdin@gmail.com>
2022-11-18 13:32:16 +0600

    Add files via upload

---
+++ DZ2_Nurdin_20-03/Part-1.js
@@ -0,0 +1,14 @@
+// var arr = [];
+// for (i = 0; i <= 10; i++) {
+//   arr.push(i);
+//   console.log(arr[i]);
+// }
+
+var arr = [];
+
+for (let i = 0; i <= 10; i++) {
+  arr.push(i);
+  console.log(arr);
+}
+
+
---
+++ DZ2_Nurdin_20-03/Part-3.js
@@ -0,0 +1,16 @@
+// let i = 0;
+// red = "красный";
+// yellow = "желтый";
+// green = "зеленый";
+// while (i == 0) {
+//   traffic = prompt("Введите цвет : ");
+//   if (traffic == red) {
+//     alert("STOP!!!");
+//   } else if (traffic == yellow) {
+//     alert("Ожидайте");
+//   } else if (traffic == green) {
+//     alert("Можете идти!");
+//   } else {
+//     alert("Введите цвет светофора!!! И желательно с маленькой буквы!!!");
+//   }
+// }
