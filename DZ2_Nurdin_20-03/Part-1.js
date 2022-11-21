ec55b79732bf8a7449f1296388cba2ad5988e281
Nurdin Djumabekov <dui.nurdin@gmail.com>
2022-11-21 21:01:35 +0600

    Add files via upload

--- DZ2_Nurdin_20-03/Part-1.js
+++ DZ2_Nurdin_20-03/Part-1.js
@@ -1,12 +1,7 @@
 // let arr = [];
-// for (i = 0; i <= 10; i++) {
+// let i = 0;
+// while (i < 10) {
+//   i++;
 //   arr.push(i);
-//   console.log(arr[i]);
 // }
-
-let arr = [];
-
-for (let i = 0; i <= 10; i++) {
-  arr.push(i);
-  console.log(arr);
-}
+// console.log(arr);
